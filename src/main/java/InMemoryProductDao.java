7fee58e059c0240e06a59e3172a91b864f22555a
MarkovaOlga <olgamarkova85@ya.ru>
2023-11-01 20:26:21 +0300

    change class

--- src/main/java/InMemoryProductDao.java
+++ src/main/java/InMemoryProductDao.java
@@ -1,8 +1,37 @@
 //Создание Data Access Object (DAO) для продуктов:
 //
-//Создайте класс ProductDAO, который будет содержать методы для добавления,
+// Создайте класс ProductDAO, который будет содержать методы для добавления,
 // удаления и получения продуктов из базы данных (или списка).
-//Используйте этот класс для взаимодействия с данными о продуктах.
+// Используйте этот класс для взаимодействия с данными о продуктах.
 
-public class InMemoryProductDao {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryProductDao implements ProductDao {
+    private List<Product> products = new ArrayList<>();
+
+    @Override
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products);
+    }
+
+    @Override
+    public void add(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public void delete(int id) {
+        products.removeIf(product -> product.getId() == id);
+    }
 }
